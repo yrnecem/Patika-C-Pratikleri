0f3c373afabdfc7fbf77c0bc3b85890c695a3bb7
Yaren Ecem Terzioğlu <86913462+yrnecem@users.noreply.github.com>
2021-12-03 22:11:18 +0300

    Add files via upload
    If Else çalışması

---
+++ CSPratik/pratiklerim/ifelse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ifelse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if(time>= 6 && time <11)
+            Console.WriteLine("Günaydın");
+            else if (time <= 18)
+            Console.WriteLine("İyi Günler");
+            else
+            Console.WriteLine("İyi geceler");
+
+        
+
+        }
+    }
+}
