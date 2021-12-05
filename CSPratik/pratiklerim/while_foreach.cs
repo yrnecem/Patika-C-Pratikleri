60b4b9a0bbd0c6bdcf458e93938610d08b6b9042
Yaren Ecem Terzioğlu <86913462+yrnecem@users.noreply.github.com>
2021-12-05 23:50:51 +0300

    Add files via upload
    While Foreach pratikleri (patika)

---
+++ CSPratik/pratiklerim/while_foreach.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //while
+           //1 den başlayarak consola girilen sayıya kadar (sayı dahil) ortalama hesaplayıp consolea yazdıran program 
+
+         //  Console.WriteLine("Lütfen Bir Sayı Giriniz");
+          // int sayi = int.Parse(Console.ReadLine());
+          // int sayac = 1;
+          // int toplam = 0;
+         //  while ( sayac <= sayi)
+          // {
+            //   toplam += sayac;
+          //     sayac ++;
+        //   }
+       //      Console.WriteLine(toplam/sayi);
+
+        // a'dan z'ye kadar tüm harfleri konsola yazdır
+
+
+      //  char charackter = 'a';
+    //    while(charackter < 'z');
+    // {
+    //     Console.Write(charackter);
+   //     charackter ++;
+      //  }
+
+        Console.Write("****Foreach****");
+        string [] arabalar = {"BMW,Ford,Toyota,Nissan"};
+
+        foreach(var araba in arabalar)
+        {
+            Console.WriteLine(araba);
+        }
+
+
+
+        }
+    }
+}
