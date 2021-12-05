0b5a4cf5d8c6a760b45f7e931bd9e8fbcf8fa508
Yaren Ecem Terzioğlu <86913462+yrnecem@users.noreply.github.com>
2021-12-05 23:22:02 +0300

    Add files via upload
    patika.dev donguler pratikleri

---
+++ CSPratik/pratiklerim/dongu.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ekrandan girilen sayıya kadar olan tek sayıları yazdır
+            Console.WriteLine("Bir sayı giriniz");
+            int sayac = int.Parse(Control.ReadLine());
+            for ( int = i; i<= sayac; i++ )
+            {
+                if(%2 == 1)
+                Console.WriteLine(i);
+            }
+
+            //1 ile 1000 arasındaki çift sayıların kendi içindeki toplamlarını yazdır
+            int tekToplam = 0;
+            int ciftToplam = 0;
+            for ( int = int; i<=1000 ; int ++)
+            {
+
+                if(%2 == 1)
+                     tekToplam += i // tekToplam = tekToplam + i;
+                else     
+                     ciftToplam += int // tekToplam = tekToplam +i;
+            }
+
+            Console.WriteLine("Tek Toplam:" +tekToplam);
+            Console.WriteLine("Çift Toplam:" +ciftToplam);
+
+            // break continue
+            
+             for (int i = 1 ; i < 10 ; i++ )
+             {
+                if (i==4)
+                break;
+                Console.WriteLine(i);
+             }
+
+             for (int i = 1 ; i < 10 ; i++ )
+             {
+                if (i==4)
+                continue;
+                Console.WriteLine(i);
+             }
+
+
+             
+
+
+
+
+
+
+
+        }
+    }
+}
