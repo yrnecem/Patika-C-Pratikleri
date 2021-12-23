f41d35e622089d5c2cbefd55101a339ce65ba327
Yaren Ecem Terzioğlu <86913462+yrnecem@users.noreply.github.com>
2021-12-23 23:53:37 +0300

    metot tanımlama eklendi

---
+++ CSPratik/pratiklerim/metotlar.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b =3;
+            
+            Console.WriteLine(a+b);
+
+            int sonuc = Topla(a,b);
+            Console.WriteLine(sonuc);
+
+            Metotlar ornek = new Metotlar();
+            ornek.EkranaYazdir(Convert.ToString(sonuc));
+
+
+        }
+
+
+        static int Topla (int deger1, int deger2)
+        {
+
+            return (deger1 + deger2);
+        }
+
+
+    }
+
+
+   class Metotlar
+
+    {
+          public void EkranaYazdir(string veri)
+          {
+              Console.WriteLine(veri);
+          }
+
+
+    }
+}
