59e63df09aadd59eb8cb6b936b4c963bb5731654
Yaren Ecem Terzioğlu <86913462+yrnecem@users.noreply.github.com>
2021-12-23 20:01:57 +0300

    Array Sınıfı Methodları eklendi

---
+++ CSPratik/pratiklerim/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace arraysinif
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+         //Sort
+
+         int[] sayiDizisi = {12,23,45,34,67,87,65};
+         
+         Console.WriteLine("****Sırasız Dizi*******");
+         foreach (var sayi in sayiDizisi)
+            Console.WriteLine(sayi);
+
+         Console.WriteLine("*****Sıralı Dizi*******");
+         Array.Sort(sayiDizisi);    
+  
+         foreach (var sayi in sayiDizisi)
+            Console.WriteLine(sayi);
+
+
+            //Clear
+
+           Console.WriteLine("****Array Clear*****");
+           //sayıDizisi elemanlarını kullanarak 2. indexten itibaren 0'lar.
+
+           Array.Clear(sayiDizisi,2,2);
+
+           
+         foreach (var sayi in sayiDizisi)
+            Console.WriteLine(sayi);
+
+            //Reverse
+            Console.WriteLine("****Array Reverse*****");
+            Array.Reverse(sayiDizisi);
+
+         foreach (var sayi in sayiDizisi)
+            Console.WriteLine(sayi);   
+         
+         
+         
+         
+         
+         
+         
+         
+         
+            
+        }
+    }
+}
