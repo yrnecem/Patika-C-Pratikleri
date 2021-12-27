cf81dc715cb6eb70457d050a92dbe4753a418972
Yaren Ecem Terzioğlu <86913462+yrnecem@users.noreply.github.com>
2021-12-27 19:12:07 +0300

    kurucu metotlar eklendi

---
+++ CSPratik/pratiklerim/kurucu_metotlar.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace kurucu_metotlar
+{
+    class kurucu_metotlar
+    {
+        static void Main(string[] args)
+        {
+           //Söz dizimi
+           //class sınıfAdı
+           // {
+           //    [Erişim Belirleyici]  [Veri Tipi] özellikAdı;
+           //    [Erişim Belirleyici]  [Geri Dönüş Tipi]  MetotAdı([Parametre Listesi])
+
+           //         {
+           //              Metot Komutları
+           //          } 
+
+
+            //Erişim Belirleyiciler
+            // *Public
+            // *Private
+            // *Internal
+            // *Protected
+
+            Calisan calisan1 = new Calisan("Yaren Ecem" , "Terzioğlu", 1905 , "İnsan Kaynakları");
+            calisan1.CalisanBilgileri();
+
+
+            Calisan calisan2 = new Calisan("Şevval", "izgördü", 2001, "İnsan Kaynakları");
+            calisan2.CalisanBilgileri();
+          
+        }
+    }
+
+    class Calisan
+
+    {
+        public string Ad;
+
+        public string soyAd;
+
+        public int No;
+
+        public string Departman;
+
+        public Calisan(string Ad, string soyAd, int No, string Departman)
+        {
+        this.Ad= Ad;
+        this.soyAd = soyAd;
+        this.No = No;
+        this.Departman = Departman;
+        }
+
+
+        public void CalisanBilgileri()
+         {
+        Console.WriteLine("Çalışan Adı:{0}", Ad);
+        Console.WriteLine("Çalışan Soyadı:{0}", soyAd);
+        Console.WriteLine("Çalışsan No:{0}", No);
+        Console.WriteLine("Çalışan Departmanı:{0}", Departman);
+         }
+    }
+
+
+
+    
+}
